ca56c7a9b1587bb5b20116a1f1908a7cd1a49d69
Vasanthan <vasanthans339@gmail.com>
2023-02-20 17:40:07 +0530

    Title has been changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-CICD';
+  title = 'CI/CD Project with Google Cloud Build';
 }
